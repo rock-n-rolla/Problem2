4e7b88e32a49cfd9ffc47af5da191b6af9df2b3e
Alexander Aldokhin <alex.aldokhin@outlook.com>
2017-07-12 20:21:56 +0700

    Solved

--- Problem2/Program.cs
+++ Problem2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace Problem2
 {
@@ -6,7 +6,28 @@ namespace Problem2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine(SumOfEvenValuedTerms(4_000_000));
+        }
+
+        static ulong SumOfEvenValuedTerms(uint hi)
+        {
+            uint previous2 = 1;
+            uint previous1 = 1;
+            uint currentEven = 2;
+
+            uint sum = currentEven;
+
+            while (true)
+            {
+                previous2 = previous1 + currentEven;
+                previous1 = previous2 + currentEven;
+                currentEven = previous1 + previous2;
+
+                if (currentEven > hi)
+                    return sum;
+
+                sum += currentEven;
+            }
         }
     }
 }
